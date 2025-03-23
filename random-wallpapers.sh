47dc960124e35d014a2cbfb5af65a397a5758a91
Yuri Miranda Fialho <mirandafialho@gmail.com>
2025-03-22 21:41:09 -0300

    Kamehameha!!!

--- random-wallpapers.sh
+++ random-wallpapers.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 DIR="$HOME/Pictures/Wallpapers"
-PIC=$(find "$DIR" type -f | shuf -n 1)
+PIC=$(find "$DIR" -name "*.jpg" -o -name "*.png" | shuf -n 1)
 
-gsettings set org.gnome.desktop.background picture-uri-dark "file://${PIC}"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$PIC"
